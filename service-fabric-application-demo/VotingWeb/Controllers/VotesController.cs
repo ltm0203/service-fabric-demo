ae08513be87fdb07e82b4f50e264aa7842edd301
角落的白板报 <werltm@hotmail.com>
2018-09-13 18:29:33 +0800

    初始化，添加无状态类的投票功能。

---
+++ service-fabric-application-demo/VotingWeb/Controllers/VotesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VotingWeb.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Votes")]
+    public class VotesController : Controller
+    {
+        public IActionResult Get()
+        {
+            List<KeyValuePair<string,int>>  votes=new List<KeyValuePair<string, int>>();
+
+            votes.Add(new KeyValuePair<string, int>("披萨",3));
+
+            votes.Add(new KeyValuePair<string, int>("冰淇淋",4));
+            return Json(votes);
+
+
+        }
+    }
+}
---
+++ service-fabric-application-demo/VotingWeb/wwwroot/js/site.js
@@ -0,0 +1,38 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+var app = angular.module('VotingApp', ['ui.bootstrap']);
+app.run(function () { });
+
+app.controller('VotingAppController', ['$rootScope', '$scope', '$http', '$timeout', function ($rootScope, $scope, $http, $timeout) {
+
+    $scope.refresh = function () {
+        $http.get('api/Votes?c=' + new Date().getTime())
+            .then(function (data, status) {
+                $scope.votes = data;
+            }, function (data, status) {
+                $scope.votes = undefined;
+            });
+    };
+
+    $scope.remove = function (item) {
+        $http.delete('api/Votes/' + item)
+            .then(function (data, status) {
+                $scope.refresh();
+            })
+    };
+
+    $scope.add = function (item) {
+        var fd = new FormData();
+        fd.append('item', item);
+        $http.put('api/Votes/' + item, fd, {
+                transformRequest: angular.identity,
+                headers: { 'Content-Type': undefined }
+            })
+            .then(function (data, status) {
+                $scope.refresh();
+                $scope.item = undefined;
+            })
+    };
+}]);
